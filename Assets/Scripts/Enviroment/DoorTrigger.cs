033e3054b0cddbee3f1a1a2581d1c017b667d0a5
Modeus <blastachanel@gmail.com>
2025-02-20 16:00:09 +0300

    Few changes

---
+++ Assets/Scripts/Enviroment/DoorTrigger.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField] private Animator doorAnimator;
+    void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
