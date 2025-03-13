997569de169a1a160ef28157804a8129a68361d6
Modeusis <blastachanel@gmail.com>
2025-03-13 20:54:07 +0300

    water minigame generator

--- Assets/Game/Scripts/MiniActivities/ManaRestoringScript.cs
+++ Assets/Game/Scripts/MiniActivities/ManaRestoringScript.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Game.Scripts.Interface;
 using TMPro;
 using UnityEngine;
@@ -8,6 +9,7 @@ using UnityEngine.Serialization;
 
 namespace Game.Scripts.MiniActivities
 {
+    //Пофиксить позицию спавна и работу INPUT
     public class ManaRestoringScript : MonoBehaviour, IToggle
     {
         [SerializeField] private GameObject player;
@@ -15,11 +17,19 @@ namespace Game.Scripts.MiniActivities
         [SerializeField] private Transform toggleTransform;
         [SerializeField] private WordsData wordsDataBase;
         
+        [SerializeField] private TMP_InputField inputWordField;
+        [SerializeField] private CanvasGroup manaRestoringCanvasGroup;
+
+        [SerializeField] private float timeGenerationDelay = 4f;
+        
         [field: SerializeField] private List<WordBlock> WordBlocks { get; set; }
         
         private bool _isFocused;
         private bool _isToggled;
         private Transform _playerTransform;
+
+        private int _wordsCapacity;
+        private int _textBlocksCapacity;
 
         public bool IsFocused
         {
@@ -46,7 +56,24 @@ namespace Game.Scripts.MiniActivities
                 if (value)
                 {
                     _playerTransform = player.transform;
+                    _wordsCapacity = wordsDataBase.Words.Count;
+                    _textBlocksCapacity = WordBlocks.Count;
                 }
+                
+                StopCoroutine(WordGenerator());
+                
+                manaRestoringCanvasGroup.gameObject.SetActive(value);
+                manaRestoringCanvasGroup.alpha = value ? 1f : 0f;
+
+                if (inputWordField && value)
+                {
+                    inputWordField.text = "";
+                    inputWordField.ActivateInputField();
+                    if (_wordsCapacity != 0 && _textBlocksCapacity != 0)
+                    {
+                        StartCoroutine(WordGenerator());
+                    }
+                }                
                 
                 player.transform.position = _isToggled ? toggleTransform.position : _playerTransform.position;
                 player.transform.rotation = _isToggled ? toggleTransform.rotation : _playerTransform.rotation;
@@ -66,12 +93,28 @@ namespace Game.Scripts.MiniActivities
                 {
                     Toggle();
                 }
+                if (Input.GetKeyDown(KeyCode.Backspace))
+                {
+                    inputWordField.text = String.Empty;
+                }
             }
         }
 
         void GenerateWords()
         {
+            var randomWordNumber = UnityEngine.Random.Range(0, _wordsCapacity);
             
+            var notShowedWordBlocks = WordBlocks.Where(block => !block.IsShowed);
+            int notShowedCount = notShowedWordBlocks.Count();
+            
+            if (notShowedCount == 0)
+                return;
+            
+            var randomTextBlockNumber = UnityEngine.Random.Range(0, notShowedCount);
+            
+            notShowedWordBlocks.ToList()[randomTextBlockNumber].TextBlockValue = wordsDataBase.Words[randomWordNumber];
+            
+            Debug.Log($"{randomTextBlockNumber} : {randomWordNumber}");
         }
 
         void RestoreMana(int manaToRestore)
@@ -79,11 +122,21 @@ namespace Game.Scripts.MiniActivities
             Player.Player.instance.CurrentMana += manaToRestore;
         }
 
+        public void InputTextChanged()
+        {
+            if (string.IsNullOrEmpty(inputWordField.text))
+                return;
+            Debug.Log($"{inputWordField.text}");
+        }
+
         IEnumerator WordGenerator()
         {
-            yield return new WaitForSeconds(2);
-            GenerateWords();
+            while (IsToggled)
+            {
+                yield return new WaitForSeconds(timeGenerationDelay);
+                GenerateWords();
+                Debug.Log("word generator called");
+            }
         }
-            
     }
 }
--- Assets/Game/Scripts/MiniActivities/WordBlock.cs
+++ Assets/Game/Scripts/MiniActivities/WordBlock.cs
@@ -1,14 +1,91 @@
 using System;
+using System.Collections;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Game.Scripts.MiniActivities
 {
-    [Serializable]
-    public class WordBlock
+    [RequireComponent(typeof(TMP_Text))]
+    public class WordBlock : MonoBehaviour
     {
-        [field:SerializeField] public TMP_Text TextBlock{ get; set; } 
-        [field:SerializeField] public bool IsShowed{ get; set; } 
-        [field:SerializeField] public string TextBlockValue{ get; set; } 
+        [field:SerializeField] private bool isShowed;
+        [field:SerializeField] private string textBlockValue;
+        
+        private TMP_Text _textBlock;
+        private Transform _rectTransform;
+        private float _randomX;
+        public bool IsShowed
+        {
+            get => isShowed;
+            private set
+            {
+                if (isShowed == value)
+                    return;
+                isShowed = value;
+                
+                _textBlock.DOKill();
+                
+                if (isShowed)
+                {
+                    _randomX = UnityEngine.Random.Range(0, Screen.width);
+                    _textBlock.DOFade(1, 0.5f);
+                    _textBlock.text = textBlockValue;
+                    StartCoroutine(TextFallingCoroutine(() =>
+                    {
+                        TextBlockValue = null;
+                    }));
+                }
+                else
+                {
+                    _textBlock.DOFade(0, 0.5f);
+                    _textBlock.rectTransform.anchoredPosition = new Vector2(_randomX, 0);
+                }
+            }
+        }
+
+        public string TextBlockValue
+        {
+            get => textBlockValue;
+            set
+            {
+                if (textBlockValue == value)
+                    return;
+                if (value != null)
+                {
+                    textBlockValue = value;
+                    
+                    IsShowed = true;
+                }
+                else
+                {
+                    IsShowed = false;
+                }
+                
+            }
+        }
+
+        IEnumerator TextFallingCoroutine(Action onComplete, float duration = 5f)
+        {
+            var startPos = _textBlock.rectTransform.anchoredPosition;
+            var finalPos = new Vector2(startPos.x, -Screen.height);
+            float timer = 0f;
+            
+            while (timer < duration)
+            {
+                var t = timer / duration;
+                _textBlock.rectTransform.anchoredPosition = Vector2.Lerp(startPos, finalPos, t);
+                timer += Time.deltaTime;
+                yield return null;
+            }
+            
+            onComplete?.Invoke();
+        }
+
+        private void Awake()
+        {
+            _textBlock = GetComponent<TMP_Text>();
+        }
     }
 }
