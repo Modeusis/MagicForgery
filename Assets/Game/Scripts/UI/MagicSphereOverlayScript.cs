a4c924403e0b10568434dfc797362b2abfd7aed3
Modeus <blastachanel@gmail.com>
2025-03-18 15:26:52 +0300

    small adjustments

--- Assets/Game/Scripts/UI/MagicSphereOverlayScript.cs
+++ Assets/Game/Scripts/UI/MagicSphereOverlayScript.cs
@@ -1,10 +1,19 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI
 {
     [RequireComponent(typeof(CanvasGroup))]
     public class MagicSphereOverlayScript : MonoBehaviour
     {
+        [SerializeField] private Button closeButton;
+        [SerializeField] private Button dismissEnchantmentButton;
+        [SerializeField] private Button startEnchantmentButton;
+        [SerializeField] private Image swordSprite;
+        [SerializeField] private TMP_Text enchantmentInfo;
+        [SerializeField] private TMP_Text swordInfo;
+        
         
     }
 }
