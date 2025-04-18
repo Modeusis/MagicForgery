fde740e5025a4326fb6f23966f39fd10b39c7d45
Modeusis <blastachanel@gmail.com>
2025-04-18 22:00:46 +0300

    fsr-, aim navigation set up

---
+++ Assets/Game/Scripts/AimMarks/AimArrowScript.cs
@@ -0,0 +1,24 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Game.Scripts.AimMarks
+{
+    public class AimArrowScript : MonoBehaviour
+    {
+        private string _currentMessage;
+        
+        [SerializeField] private AimMarksConfig aimMarksConfig;
+        
+        [SerializeField] private TMP_Text stepText;
+            
+        private void ShowMessage()
+        {
+            
+        }
+
+        public void SwitchAim(MarkType aimType)
+        {
+            
+        }
+    }
+}
---
+++ Assets/Game/Scripts/AimMarks/AimMarksConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Scripts.AimMarks
+{
+    public class AimMarksConfig : ScriptableObject
+    {
+        [field: SerializeField] public MarkType Aim {get; private set;}
+        
+        [field: SerializeField] public Transform AimTransform {get; private set;}
+        
+        [field: SerializeField] public List<string> MessagesForStep {get; private set;}
+    }
+}
